b8283e63314e97dc26cb68aacccb514987a1a6bc
Artem Volok <artiom.volok@gmail.com>
2024-01-10 23:24:26 +0200

    [FE-create-feedback-page] code rewiew

--- frontend/src/pages/Feedback/index.tsx
+++ frontend/src/pages/Feedback/index.tsx
@@ -1,27 +1,8 @@
 import { Link } from 'react-router-dom';
-import { DashboardUrl } from '../../UrlsConfig';
-import './style.scss';
 import { useForm } from 'react-hook-form';
-
-interface IFeedbackForm {
-  gender: string;
-  age: number;
-  musicInfluence: number;
-  totalImpression: number;
-  generalAppearance: number;
-  designAppeal: number;
-  colorPalette: number;
-  fontCompatibility: number;
-  searchInfo: number;
-  organizationNavigation: number;
-  usabilityAdaptive: number;
-  useOnMobile: number;
-  experienceWithMusic: number;
-  timeWithMusic: number;
-  expressionWithMusic: number;
-}
-
-const arrGrade: number[] = [1, 2, 3, 4, 5];
+import { DashboardUrl } from '../../UrlsConfig';
+import { IFeedbackForm, IQuestion, arrGrade, listQuestion } from './types';
+import './style.scss';
 
 const Feedback = () => {
   const { handleSubmit, register } = useForm<IFeedbackForm>({
@@ -103,308 +84,33 @@ const Feedback = () => {
               />
             </div>
           </div>
-          <div className="musicInfluence">
-            <h4>
-              Чи вплинула фонова музика на вашу загальну атмосферу під час
-              перегляду сайту?
-            </h4>
-            <div>
-              {arrGrade.map((el: number) => {
-                return (
-                  <label
-                    htmlFor={`musicInfluence${el}`}
-                    key={`musicInfluence${el}`}
-                  >
-                    <p>{el}</p>
-                    <input
-                      {...register('musicInfluence')}
-                      type="radio"
-                      value={el}
-                      id={`musicInfluence${el}`}
-                    />
-                    <span className="checkMark"></span>
-                  </label>
-                );
-              })}
-            </div>
-          </div>
-          <div className="totalImpression">
-            <h4>
-              Як ви оцінюєте загальний враження від візуального та аудіо
-              дизайну?
-            </h4>
-            <div>
-              {arrGrade.map((el: number) => {
-                return (
-                  <label
-                    htmlFor={`totalImpression${el}`}
-                    key={`totalImpression${el}`}
-                  >
-                    {el}
-                    <input
-                      {...register('totalImpression')}
-                      type="radio"
-                      value={el}
-                      id={`totalImpression${el}`}
-                    />
-                    <span className="checkMark"></span>
-                  </label>
-                );
-              })}
-            </div>
-          </div>
+          {listQuestion.map((el: IQuestion) => {
+            return (
+              <div key={el.nameQuestion}>
+                <h4>{el.question}</h4>
+                <div>
+                  {arrGrade.map((count: number) => {
+                    return (
+                      <label
+                        htmlFor={`${el.nameQuestion}${count}`}
+                        key={`${el.nameQuestion}${count}`}
+                      >
+                        <p>{count}</p>
+                        <input
+                          {...register(el.nameQuestion as keyof IFeedbackForm)}
+                          type="radio"
+                          value={count}
+                          id={`${el.nameQuestion}${count}`}
+                        />
+                        <span className="checkMark"></span>
+                      </label>
+                    );
+                  })}
+                </div>
+              </div>
+            );
+          })}
 
-          <div className="generalAppearance">
-            <h4>Оцініть загальний зовнішній вигляд веб-додатку</h4>
-            <div>
-              {arrGrade.map((el: number) => {
-                return (
-                  <label
-                    htmlFor={`generalAppearance${el}`}
-                    key={`generalAppearance${el}`}
-                  >
-                    {el}
-                    <input
-                      {...register('generalAppearance')}
-                      type="radio"
-                      value={el}
-                      id={`generalAppearance${el}`}
-                    />
-                    <span className="checkMark"></span>
-                  </label>
-                );
-              })}
-            </div>
-          </div>
-          <div className="designAppeal">
-            <h4>Як ви оцінюєте привабливість та сучасність дизайну сайту?</h4>
-            <div>
-              {arrGrade.map((el: number) => {
-                return (
-                  <label
-                    htmlFor={`designAppeal${el}`}
-                    key={`designAppeal${el}`}
-                  >
-                    {el}
-                    <input
-                      {...register('designAppeal')}
-                      type="radio"
-                      value={el}
-                      id={`designAppeal${el}`}
-                    />
-                    <span className="checkMark"></span>
-                  </label>
-                );
-              })}
-            </div>
-          </div>
-          <div className="colorPalette">
-            <h4>
-              Чи добре ви сприймаєте колірну палітру та засоби візуальної
-              ідентифікації сайту?
-            </h4>
-            <div>
-              {arrGrade.map((el: number) => {
-                return (
-                  <label
-                    htmlFor={`colorPalette${el}`}
-                    key={`colorPalette${el}`}
-                  >
-                    {el}
-                    <input
-                      {...register('colorPalette')}
-                      type="radio"
-                      value={el}
-                      id={`colorPalette${el}`}
-                    />
-                    <span className="checkMark"></span>
-                  </label>
-                );
-              })}
-            </div>
-          </div>
-          <div className="fontCompatibility">
-            <h4>
-              Як ви оцінюєте відповідність шрифту і кольорової гами в дизайні до
-              тематики сайту?
-            </h4>
-            <div>
-              {arrGrade.map((el: number) => {
-                return (
-                  <label
-                    htmlFor={`fontCompatibility${el}`}
-                    key={`fontCompatibility${el}`}
-                  >
-                    {el}
-                    <input
-                      {...register('fontCompatibility')}
-                      type="radio"
-                      value={el}
-                      id={`fontCompatibility${el}`}
-                    />
-                    <span className="checkMark"></span>
-                  </label>
-                );
-              })}
-            </div>
-          </div>
-          <div className="searchInfo">
-            <h4>Чи легко ви знаходите потрібну інформацію на сайті?</h4>
-            <div>
-              {arrGrade.map((el: number) => {
-                return (
-                  <label htmlFor={`searchInfo${el}`} key={`searchInfo${el}`}>
-                    {el}
-                    <input
-                      {...register('searchInfo')}
-                      type="radio"
-                      value={el}
-                      id={`searchInfo${el}`}
-                    />
-                    <span className="checkMark"></span>
-                  </label>
-                );
-              })}
-            </div>
-          </div>
-          <div className="organizationNavigation">
-            <h4>Чи задоволені ви організацією навігації на сайті?</h4>
-            <div>
-              {arrGrade.map((el: number) => {
-                return (
-                  <label
-                    htmlFor={`organizationNavigation${el}`}
-                    key={`organizationNavigation${el}`}
-                  >
-                    {el}
-                    <input
-                      {...register('organizationNavigation')}
-                      type="radio"
-                      value={el}
-                      id={`organizationNavigation${el}`}
-                    />
-                    <span className="checkMark"></span>
-                  </label>
-                );
-              })}
-            </div>
-          </div>
-          <div className="usabilityAdaptive">
-            <h4>
-              Як ви вважаєте, чи полегшує адаптивний дизайн сайту вам процес
-              вибору та порівняння мотоциклів на різних пристроях?
-            </h4>
-            <div>
-              {arrGrade.map((el: number) => {
-                return (
-                  <label
-                    htmlFor={`usabilityAdaptive${el}`}
-                    key={`usabilityAdaptive${el}`}
-                  >
-                    {el}
-                    <input
-                      {...register('usabilityAdaptive')}
-                      type="radio"
-                      value={el}
-                      id={`usabilityAdaptive${el}`}
-                    />
-                    <span className="checkMark"></span>
-                  </label>
-                );
-              })}
-            </div>
-          </div>
-          <div className="useOnMobile">
-            <h4>
-              Як ви оцінюєте зручність використання сайту на мобільних
-              пристроях?
-            </h4>
-            <div>
-              {arrGrade.map((el: number) => {
-                return (
-                  <label htmlFor={`useOnMobile${el}`} key={`useOnMobile${el}`}>
-                    {el}
-                    <input
-                      {...register('useOnMobile')}
-                      type="radio"
-                      value={el}
-                      id={`useOnMobile${el}`}
-                    />
-                    <span className="checkMark"></span>
-                  </label>
-                );
-              })}
-            </div>
-          </div>
-          <div className="experienceWithMusic">
-            <h4>
-              Чи вважаєте ви, що фонова музика покращує загальний
-              користувальницький досвід на сайті?
-            </h4>
-            <div>
-              {arrGrade.map((el: number) => {
-                return (
-                  <label
-                    htmlFor={`experienceWithMusic${el}`}
-                    key={`experienceWithMusic${el}`}
-                  >
-                    {el}
-                    <input
-                      {...register('experienceWithMusic')}
-                      type="radio"
-                      value={el}
-                      id={`experienceWithMusic${el}`}
-                    />
-                    <span className="checkMark"></span>
-                  </label>
-                );
-              })}
-            </div>
-          </div>
-          <div className="timeWithMusic">
-            <h4>Чи вплинула музика на вашу тривалість перебування на сайті?</h4>
-            <div>
-              {arrGrade.map((el: number) => {
-                return (
-                  <label
-                    htmlFor={`timeWithMusic${el}`}
-                    key={`timeWithMusic${el}`}
-                  >
-                    {el}
-                    <input
-                      {...register('timeWithMusic')}
-                      type="radio"
-                      value={el}
-                      id={`timeWithMusic${el}`}
-                    />
-                    <span className="checkMark"></span>
-                  </label>
-                );
-              })}
-            </div>
-          </div>
-          <div className="expressionWithMusic">
-            <h4>Оцініть вплив сайту на ваше враження про продукт</h4>
-            <div>
-              {arrGrade.map((el: number) => {
-                return (
-                  <label
-                    htmlFor={`expressionWithMusic${el}`}
-                    key={`expressionWithMusic${el}`}
-                  >
-                    {el}
-                    <input
-                      {...register('expressionWithMusic')}
-                      type="radio"
-                      value={el}
-                      id={`expressionWithMusic${el}`}
-                    />
-                    <span className="checkMark"></span>
-                  </label>
-                );
-              })}
-            </div>
-          </div>
           <div className="submitSector">
             <Link to={DashboardUrl} className="submitSector__link">
               На головну
